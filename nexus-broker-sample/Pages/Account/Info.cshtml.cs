93ccfdb37b01ceb86b471051f55adc2ff4bb5bfb
Raymen Scholten <raymenscholten@outlook.com>
2020-04-03 12:23:12 +0200

    Improve error handling for fetching account info

--- nexus-broker-sample/Pages/Account/Info.cshtml.cs
+++ nexus-broker-sample/Pages/Account/Info.cshtml.cs
@@ -27,9 +27,39 @@ namespace Nexus.Samples.Broker.Pages.Account
         {
             if (!string.IsNullOrWhiteSpace(AccountCode))
             {
-                var account = await nexusClient.GetAccount(AccountCode);
+                var accountResult = await nexusClient.GetAccount(AccountCode);
 
-                var transactions = await nexusClient.GetTransactions(account.Values.CustomerCode);
+                if (!accountResult.IsSuccess)
+                {
+                    foreach (var error in accountResult.Errors)
+                    {
+                        switch (error)
+                        {
+                            case "AccountNotFound": ModelState.AddModelError(nameof(AccountCode), "Account not found."); break;
+                            default:
+                                break;
+                        }
+                    }
+
+                    return Page();
+                }
+
+                var transactions = await nexusClient.GetTransactions(accountResult.Values.CustomerCode);
+
+                if (!transactions.IsSuccess)
+                {
+                    foreach (var error in transactions.Errors)
+                    {
+                        switch (error)
+                        {
+                            //case "AccountNotFound": ModelState.AddModelError(nameof(AccountCode), "Account not found."); break;
+                            default:
+                                break;
+                        }
+                    }
+
+                    return Page();
+                }
 
                 Transactions = transactions.Values.Records;
             }
