25cc3b9727cbd7d5723fedb2231088754547de9d
Maximilian Stoll <max@quantoz.com>
2020-08-06 17:57:09 +0200

    Implemented two step delete functionality

--- nexus-broker-sample/Pages/Account/Deleted.cshtml.cs
+++ nexus-broker-sample/Pages/Account/Deleted.cshtml.cs
@@ -1,9 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Nexus.Samples.Sdk;
+using Nexus.Samples.Sdk.Models.Request;
 using Nexus.Samples.Sdk.Models.Response;
+using Nexus.Samples.Sdk.Models.Shared;
 
 namespace Nexus.Samples.Broker.Pages.Account
 {
@@ -14,6 +17,11 @@ namespace Nexus.Samples.Broker.Pages.Account
         [BindProperty]
         [Required]
         public string AccountCode { get; set; }
+
+        public bool? SuccessfullyProcessedRequest { get; set; } = false;
+
+        [BindProperty(SupportsGet = true)]
+        public string Code { get; set; }
 
         public GetAccountResponse Account { get; private set; }
 
@@ -29,16 +37,75 @@ namespace Nexus.Samples.Broker.Pages.Account
                 return Page();
             }
 
-            var accountResult = await nexusClient.GetAccount(AccountCode);
+            var mailResponse = await nexusClient.GetMailByCode(Code);
 
-            if (!accountResult.IsSuccess)
+            if (mailResponse.IsSuccess)
             {
-                return BadRequest();
+                var mail = mailResponse.Values;
+                AccountCode = mail.References.AccountCode;
+            }
+            else
+            {
+                AccountCode = Code;
             }
 
-            Account = accountResult.Values;
+            var deleteAccountRequest = await nexusClient.DeleteAccount(AccountCode);
 
-            return Page();
+            if (deleteAccountRequest.IsSuccess)
+            {
+                SuccessfullyProcessedRequest = true;
+
+                var accountResponse = await nexusClient.GetAccount(AccountCode);
+
+                if (accountResponse.IsSuccess)
+                {
+                    var customerResponse = await nexusClient.GetCustomer(accountResponse.Values.CustomerCode);
+
+                    if (customerResponse.IsSuccess)
+                    {
+                        var activateAccountMail = new CreateMailRequest()
+                        {
+                            Type = "AccountDeletedByRequest",
+                            References = new MailEntityCodes()
+                            {
+                                AccountCode = AccountCode,
+                                CustomerCode = customerResponse.Values.CustomerCode
+                            },
+                            Recipient = new MailRecipient()
+                            {
+                                Email = customerResponse.Values.Email
+                            }
+                        };
+
+                        var deleteAccountMailResponse = await nexusClient.CreateMail(activateAccountMail);
+
+                        if (!deleteAccountMailResponse.IsSuccess)
+                        {
+                            Console.WriteLine("Failed to send AccountDeletedByRequest mail");
+                        }
+                    }
+                }
+
+                Account = accountResponse.Values;
+
+                return Page();
+            }
+            else
+            {
+                foreach (var error in deleteAccountRequest.Errors)
+                {
+                    switch (error)
+                    {
+                        case "AccountNotFound": return NotFound();
+                        default:
+                            break;
+                    }
+                }
+
+                SuccessfullyProcessedRequest = false;
+
+                return Page();
+            }
         }
     }
 }
