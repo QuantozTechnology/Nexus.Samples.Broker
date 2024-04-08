6e06d9c5961dad7c716b85ef1d2abf4bc9536445
Wayne Robért <wayne@quantoz.com>
2024-04-08 09:12:38 +0200

    Fixed rename

--- Nexus.Samples.MailService/MailFunctions.cs
+++ Nexus.Samples.MailService/MailFunctions.cs
@@ -2,7 +2,7 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Nexus.Samples.MailClient;
 
-namespace FunctionApp1
+namespace MailService
 {
     public class MailFunctions
     {
